eaf3a5afc33c112358f1f8689597374837ea64c8
albert-yw-lin <as24999788@gmail.com>
2023-04-08 16:37:41 +0800

    add jetcam

--- test.py
+++ test.py
@@ -26,62 +26,76 @@
 # NVIDIA Jetson Nano Developer Kit using OpenCV
 # Drivers for the camera and OpenCV are included in the base image
 
-import cv2
+
 
 # gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
 # Defaults to 1280x720 @ 60fps
 # Flip the image by setting the flip_method (most common values: 0 and 2)
 # display_width and display_height determine the size of the window on the screen
 
-
-def gstreamer_pipeline(
-    capture_width=640,
-    capture_height=360,
-    display_width=640,
-    display_height=360,
-    framerate=60,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
-
-def show_camera():
-    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline(flip_method=0))
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-    if cap.isOpened():
-        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
-        # Window
-        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
-            ret_val, img = cap.read()
-            cv2.imshow("CSI Camera", img)
-            # This also acts as
-            keyCode = cv2.waitKey(30) & 0xFF
-            # Stop the program on the ESC key
-            if keyCode == 27:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
-    else:
-        print("Unable to open camera")
+# import cv2
+# def gstreamer_pipeline(
+#     capture_width=640,
+#     capture_height=360,
+#     display_width=640,
+#     display_height=360,
+#     framerate=60,
+#     flip_method=0,
+# ):
+#     return (
+#         "nvarguscamerasrc ! "
+#         "video/x-raw(memory:NVMM), "
+#         "width=(int)%d, height=(int)%d, "
+#         "format=(string)NV12, framerate=(fraction)%d/1 ! "
+#         "nvvidconv flip-method=%d ! "
+#         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+#         "videoconvert ! "
+#         "video/x-raw, format=(string)BGR ! appsink"
+#         % (
+#             capture_width,
+#             capture_height,
+#             framerate,
+#             flip_method,
+#             display_width,
+#             display_height,
+#         )
+#     )
 
 
-if __name__ == "__main__":
-    show_camera()
+# def show_camera():
+#     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+#     print(gstreamer_pipeline(flip_method=0))
+#     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+#     if cap.isOpened():
+#         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+#         # Window
+#         while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+#             ret_val, img = cap.read()
+#             cv2.imshow("CSI Camera", img)
+#             # This also acts as
+#             keyCode = cv2.waitKey(30) & 0xFF
+#             # Stop the program on the ESC key
+#             if keyCode == 27:
+#                 break
+#         cap.release()
+#         cv2.destroyAllWindows()
+#     else:
+#         print("Unable to open camera")
+
+
+# if __name__ == "__main__":
+#     show_camera()
+
+##############
+### Jetcam ###
+##############
+import cv2
+import numpy as np
+from jetcam.jetcam.csi_camera import CSICamera
+
+camera = CSICamera(width=224, height=224, capture_width=1080, capture_height=720, capture_fps=30)
+while True:
+    image = camera.read()
+    cv2.imshow("123", image)
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
