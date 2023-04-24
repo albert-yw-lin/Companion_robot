e9550566fb00916c59cc57986e82efd56427b65c
albert-yw-lin <as24999788@gmail.com>
2023-04-25 01:25:28 +0800

    awdwa

--- video_chat/utils.py
+++ video_chat/utils.py
@@ -54,11 +54,11 @@ def recv_image(socket,):
 
 def gstreamer_pipeline(
     sensor_id=0,
-    capture_width=VIDEO_WIDTH,
-    capture_height=VIDEO_HEIGHT,
-    display_width=VIDEO_WIDTH,
-    display_height=VIDEO_HEIGHT,
-    framerate=20,
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
     flip_method=0,
 ):
     return (
