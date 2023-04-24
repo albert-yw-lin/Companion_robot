20f5142428c7d621923d8d41f105907bdf9a57a6
albert-yw-lin <as24999788@gmail.com>
2023-04-25 02:00:03 +0800

    asdfg

--- video_chat/utils.py
+++ video_chat/utils.py
@@ -50,7 +50,7 @@ def recv_image(socket,):
             image = np.frombuffer(encode_image, dtype=np.uint8)
             image = cv2.imdecode(image, cv2.IMREAD_COLOR)
             cv2.imshow('recv_image', image)
-            cv2.waitKey(100)
+            cv2.waitKey(33)
 
 def gstreamer_pipeline(
     sensor_id=0,
