bfc33004f45a03a1f5c9043786b26e38e33718a4
albert-yw-lin <as24999788@gmail.com>
2023-05-07 16:50:28 +0800

    dfghj

--- test_code/A1_16_test.cpp
+++ test_code/A1_16_test.cpp
@@ -8,11 +8,11 @@ int main(){
     unsigned int pos = 0;
     
     A1_16_Ini();
-    for (pos=0; pos<=1023; pos++){
-        // std::cout << ReadPosition(id);
-        SetPositionI_JOG(id,time,pos);
-    }
-    // SetPositionI_JOG(id,time,pos);
+    // for (pos=0; pos<=1023; pos++){
+    //     // std::cout << ReadPosition(id);
+    //     SetPositionI_JOG(id,time,pos);
+    // }
+    SetPositionI_JOG(id,time,pos);
     uart_close();
     return 0;
 }
