717a5211521312520b0fdf35953e3c291b83a8b2
albert-yw-lin <as24999788@gmail.com>
2023-05-07 17:05:17 +0800

    wertyui

--- test_code/A1_16_test.cpp
+++ test_code/A1_16_test.cpp
@@ -7,15 +7,18 @@
 
 int main(){
     unsigned char id = 1;
-    unsigned char time = 100;
+    unsigned char time = 50;
     unsigned int pos = 0;
+    unsigned int now_pos;
     
     A1_16_Ini();
-    SetPositionI_JOG(id,time,pos);
-    std::this_thread::sleep_for(std::chrono::milliseconds(time*10));
-    for (pos=0; pos<=1023; pos+=100){
+    SetPositionI_JOG(id,255,pos);
+    std::this_thread::sleep_for(std::chrono::milliseconds(255));
+    for (pos=0; pos<=1023; pos+=50){
         // std::cout << ReadPosition(id);
         SetPositionI_JOG(id,time,pos);
+        now_pos = ReadPosition(id);
+        std::cout << now_pos;
         std::this_thread::sleep_for(std::chrono::milliseconds(time*10));
     }
     // SetPositionI_JOG(id,time,pos);
