8e6c4d0252d5ada5d2ca7274ea378dcbc4570b83
albert-yw-lin <as24999788@gmail.com>
2023-05-07 15:34:29 +0800

    remove A1_16 test for loop to a simpler position test

--- test_code/A1_16_test.cpp
+++ test_code/A1_16_test.cpp
@@ -3,8 +3,9 @@
 
 int main(){
     A1_16_Ini();
-    for (int i=0; i<=1023; i++){
-        SetPositionI_JOG(0,1,i);
-    }
+    // for (int i=0; i<=1023; i++){
+    //     SetPositionI_JOG(0,1,i);
+    // }
+    SetPositionI_JOG(0,3,1023);
     return 0;
 }
