dcc3040aa2da329cff96b26c1f4b2531896cda23
albert-yw-lin <as24999788@gmail.com>
2023-05-07 15:55:14 +0800

    change A1_16 test datatype to uint8_t

--- test_code/A1_16_test.cpp
+++ test_code/A1_16_test.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <cstdint>
 #include "A1_16.h"
 
 int main(){
+    uint8_t id = 1;
+    uint8_t time = 3;
+    uint8_t pos = 500;
     A1_16_Ini();
     // for (int i=0; i<=1023; i++){
     //     SetPositionI_JOG(0,1,i);
     // }
-    SetPositionI_JOG('1','3',500);
+    SetPositionI_JOG(id,time,pos);
     return 0;
 }
