b9dc402e77f3af803bc787dc08448875466cb434
albert-yw-lin <as24999788@gmail.com>
2023-05-15 20:32:18 +0800

    add a test script about face tracking

---
+++ test_code/dynamixel/face_tracking.py
@@ -0,0 +1,82 @@
+from dynamixel import Dynamixel
+import mediapipe as mp
+import numpy as np
+import cv2
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1640,
+    capture_height=1232,
+    display_width=640,
+    display_height=480,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+def face_position(results):
+    # only select the first face
+    detection = results.detections[0]
+    box = detection.location_data.relative_bounding_box
+    return (box.xmin+0.5*box.width, box.ymin+0.5*box.height)
+
+
+motor = Dynamixel()
+mp_face = mp.solutions.face_detection
+cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+
+
+
+with mp_face.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5) as face:
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            ### If loading a video, use 'break' instead of 'continue'.
+            continue
+        
+        ########################
+        ### image processing ###
+        ########################
+
+        ### To improve performance, optionally mark the image as not writeable to
+        ### pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        ### get results from face detection and pose
+        results = face.process(image)
+
+        ### turn the image into writable and BGR mode
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        ### face position calculation
+        if results.detections: face_center_x, face_center_y = face_position(results)
+        error_x, error_y = face_center_x-0.5, face_center_y-0.5
+        add_motor_x, add_motor_y = int(error_x*(62.2/360)*4095), int(error_y*(48.8/360)*4095)
+        motor_pos = motor.sync_read_pos()
+        motor_pos[0] += add_motor_x
+        motor_pos[1] += add_motor_y
+        motor.sync_write_pos(motor_pos)
+
+        ### Flip the image horizontally for a selfie-view display.
+        image = cv2.flip(image, 1)
