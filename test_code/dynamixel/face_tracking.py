8bd3b372884b631c0531afdbf1866e024dff05f4
albert-yw-lin <as24999788@gmail.com>
2023-05-15 23:26:00 +0800

    modify parameter

--- test_code/dynamixel/face_tracking.py
+++ test_code/dynamixel/face_tracking.py
@@ -71,7 +71,7 @@ with mp_face.FaceDetection(
 
         ### face position calculation
         if results.detections: face_center_x, face_center_y = face_position(results)
-        error_x, error_y = face_center_x-0.5, face_center_y-0.5
+        error_x, error_y = 0.5-face_center_x, face_center_y-0.5
         add_motor_x, add_motor_y = int(error_x*(62.2/360)*4095), int(error_y*(48.8/360)*4095)
         motor_pos = motor.sync_read_pos()
         motor_pos[0] += add_motor_x
