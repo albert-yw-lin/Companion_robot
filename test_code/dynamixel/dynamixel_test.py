77d08ef4d679eb1bd27512970f61f4d255df0d09
albert-yw-lin <as24999788@gmail.com>
2023-05-20 19:07:23 +0800

    asdfgh

--- test_code/dynamixel/dynamixel_test.py
+++ test_code/dynamixel/dynamixel_test.py
@@ -3,7 +3,7 @@ from config import *
 import time
 
 motor = Dynamixel()
-motor.sync_write_pos([500,500,500,500,500,500])
-time.sleep(5)
-pos = motor.sync_read_pos()
-print(pos)
+while(1):
+
+    pos = motor.sync_read_pos()
+    print(pos)
