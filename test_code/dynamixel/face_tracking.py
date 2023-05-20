23f4be6d4c3db5d4dd774b839384f732c5ec7e07
albert-yw-lin <as24999788@gmail.com>
2023-05-20 19:50:01 +0800

    qwertyui

--- test_code/dynamixel/face_tracking.py
+++ test_code/dynamixel/face_tracking.py
@@ -73,7 +73,9 @@ with mp_face.FaceDetection(
         ### face position calculation
         if results.detections: face_center_x, face_center_y = face_position(results)
         error_x, error_y = FACE_CENTER_X-face_center_x, face_center_y-FACE_CENTER_Y
-        add_motor_x, add_motor_y = int(error_x*(62.2/360)*4095*X_P_GAIN), int(error_y*(48.8/360)*4095*Y_P_GAIN)
+        add_motor_x, add_motor_y = int(error_x*(62.2/360)*4095*P_GAIN_X), 
+        if(error_y>=0):add_motor_y = int(error_y*(48.8/360)*4095*P_GAIN_Y_DOWN)
+        else:add_motor_y = int(error_y*(48.8/360)*4095*P_GAIN_Y_UP)
         motor_pos = motor.sync_read_pos()
         motor_pos[0] += add_motor_x
         motor_pos[1] += add_motor_y
