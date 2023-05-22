d98426ea00a778bcd85b00b34b8ffe87efc5e11b
albert-yw-lin <as24999788@gmail.com>
2023-05-22 23:11:37 +0800

    add_jetson_ws

---
+++ jetson_ws/build/video_chat/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yuwei/Desktop/companion_robot/jetson_ws/src/video_chat
+# Build directory: /home/yuwei/Desktop/companion_robot/jetson_ws/build/video_chat
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
