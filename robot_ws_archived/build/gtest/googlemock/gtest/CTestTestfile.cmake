c3e2e5596a2a2aeb5365b661f143015b116bc547
albert-yw-lin <as24999788@gmail.com>
2023-11-29 18:22:11 +0800

    add gitignore

--- robot_ws_archived/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/yuwei/Desktop/companion_robot/robot_ws/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- robot_ws_archived/build/video_chat/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuwei/Desktop/companion_robot/robot_ws/src/video_chat
-# Build directory: /home/yuwei/Desktop/companion_robot/robot_ws/build/video_chat
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
